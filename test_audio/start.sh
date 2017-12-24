afcd0089523482a7b99443cd9e97e58d09c12179
jerivasuaq <jerivas@uaq.edu.mx>
2017-12-24 12:59:50 -0600

    Changed to debian:stretch-slim

--- test_audio/start.sh
+++ test_audio/start.sh
@@ -1,8 +1,14 @@
+
+
+
+SND="--device /dev/snd 
+	-e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native 
+	-v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native 
+	-v $HOME/.config/pulse/cookie:/root/.config/pulse/cookie 
+	--group-add $(getent group audio | cut -d: -f3) 
+	"
+
 docker run -it \
-	--device /dev/snd \
-	-e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
-	-v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
-	-v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
-	--group-add $(getent group audio | cut -d: -f3) \
-	jerivas/test_sound aplay /usr/share/sounds/alsa/Front_Center.wav 
+		$SND \
+		jerivas/test_sound aplay /usr/share/sounds/alsa/Front_Center.wav 
 
